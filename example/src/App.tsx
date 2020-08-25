496d0e5d0795ac5f364f6d192eb105079e639f2f
kazuooooo <matsumotokazuya7@gmail.com>
2020-08-25 17:05:49 +0900

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'fire-form'
+import 'fire-form/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
